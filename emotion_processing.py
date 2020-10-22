f81cf8e042efc4bf890c831d0464abe0fb183f53
Elias Oliveira <63745733+EliasOlie@users.noreply.github.com>
2020-10-22 16:52:36 -0300

    Commit on master

--- emotion_processing.py
+++ emotion_processing.py
@@ -30,58 +30,39 @@ def nlu_instance(phrase):
                 unknow_words.append(1)
                 len_sentimento.append(0)
             
+                
+            score = sum(len_sentimento)  
+
             
-            score = sum(len_sentimento)
-    
-    if type(phrase) == list:
-        
-        for single_phrase in phrase:
-            
-            single_phrase = single_phrase.lower()
-            len_sentimento = []
-            unknow_words = []
-
-            for word in single_phrase.split():
-
-                total_words = len(single_phrase.split())
-                actual_word = dados.get(word)
-                
-                if actual_word is not None:
-                    
-                    for key, value in actual_word.items():
-                        
-                        temp = [key,value]
-                        len_sentimento.append(temp[1])
-
-                
-                else:
-                    
-                    len_sentimento.append(0)
-                    unknow_words.append(1)
-
-                
-                score = sum(len_sentimento)
-
+    else:
+        print('Apenas string! ')
     
     
 
     try:
-        confidence_index = f'{round(total_words-sum(unknow_words)/score*100)}%'
+        confidence_index = f'{2*(score*sum(len_sentimento)/total_words)*100}%'
     except:
-        confidence_index = f'Ops! {sys.exc_info()[0]} Nenhuma palavra no banco de dados'
+        confidence_index = f'Ops! {sys.exc_info()[0]} Nenhuma palavra no banco de dados, ou provavelmente o resultado \ndo índice foi anlunado com 2 palavas positivas e 2 negativas'
 
 
 
     if score > 0:
-         msg = f'\nResultado da análise:Positvo\nScore = {score}\nIndice de confiança: {confidence_index}\nQuantidades de palavras: {total_words}\nPalavras desconhecidas: {sum(unknow_words)}'
+        msg = f'\nResultado da análise:Positvo\nScore = {score}\nIndice de confiança: {confidence_index}\nQuantidades de palavras: {total_words}\nPalavras desconhecidas: {sum(unknow_words)}'
     
     elif score == 0:
-         msg = f'\nResultado da análise: Neutro\nScore = {score}\nIndice de confiança: {confidence_index}\nQuantidades de palavras: {total_words}\nPalavras desconhecidas: {sum(unknow_words)}'
+        msg = f'\nResultado da análise: Neutro\nScore = {score}\nIndice de confiança: {confidence_index}\nQuantidades de palavras: {total_words}\nPalavras desconhecidas: {sum(unknow_words)}'
     
     else:
-         msg = f'\nResultado da análise: Negativo\nScore = {score}\nIndice de confiança: {confidence_index}\nQuantidades de palavras: {total_words}\nPalavras desconhecidas: {sum(unknow_words)}'
+        msg = f'\nResultado da análise: Negativo\nScore = {score}\nIndice de confiança: {confidence_index}\nQuantidades de palavras: {total_words}\nPalavras desconhecidas: {sum(unknow_words)}'
 
-    return [score, confidence_index, total_words, unknow_words, msg]
+    
+    return [{"Score":score,
+    "Confidence":confidence_index, 
+    "Numero de palavras":total_words, 
+    "Palavras desconhecidas":unknow_words, 
+    "Mensagem":msg,}]
 
-analys = nlu_instance('consistente feliz triste abafado')
-print(analys[4])
+analys = nlu_instance('Esta muito abafado hoje')
+
+
+print(analys[0]["Mensagem"])
