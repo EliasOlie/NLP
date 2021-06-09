2eb6a538cef52e9cd39cf7daeb12fa1f6d92dbbd
Elias Oliveira <63745733+EliasOlie@users.noreply.github.com>
2021-06-09 00:42:07 -0300

    First front-end commit

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const { json } = require('body-parser');
 const express = require('express');
+const path = require('path')
 
 var app = express();
 
 app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static(__dirname + '/pages'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/pages/index.html'))
+})
 
 app.post('/phrase', (req, res) =>{
+
     
     var spawn = require('child_process').spawn
     const phrase = req.body;
@@ -15,9 +22,10 @@ app.post('/phrase', (req, res) =>{
     process.stdout.on('data', function (data) {
         
         res.send(data.toString())
-        
+
         
     })
+
 });
 
 app.listen(process.env.PORT || 8880);
